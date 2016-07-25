37eb5e0aea1442bef9dbf632573eda063559d65f
Denis Ivanchenko <ivanchenkodenis@gmail.com>
2016-07-25 17:17:51 +0300

    new servers

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ setInterval(() => {
   storage.init()
     .then(() => {
       superagent
-        .get(`http://limitless-dusk-20243.herokuapp.com/raw_data`)
+        .get(`https://safe-peak-42852.herokuapp.com/raw_data`)
         .end(async (err, res) => {
           if (err) {
             console.log(err)
@@ -28,6 +28,72 @@ setInterval(() => {
 
           if (newPokemons > 0) {
             console.log(`${Date()} Added ${newPokemons} new pokemon(s)`)
+          }
+
+          storage.close()
+        })
+    })
+}, 60000)
+
+setInterval(() => {
+  let storage = new MongoStorage()
+  storage.init()
+    .then(() => {
+      superagent
+        .get(`https://testpokemong.herokuapp.com/raw_data`)
+        .end(async (err, res) => {
+          if (err) {
+            console.log(err)
+            return
+          }
+
+          let { pokemons } = res.body
+          let newPokemons = 0
+
+          for (let pokemon of pokemons) {
+            let found = await storage.findBy('pokemon_appears', 'encounter_id', pokemon.encounter_id)
+            if (found.length === 0) {
+              newPokemons++
+              await storage.insert('pokemon_appears', pokemon)
+            }
+          }
+
+          if (newPokemons > 0) {
+            console.log(`${Date()} Added ${newPokemons} new pokemon(s) FROM testpokemong`)
+          }
+
+          storage.close()
+        })
+    })
+}, 60000)
+
+setInterval(() => {
+  let storage = new MongoStorage()
+  storage.init()
+    .then(() => {
+      superagent
+        .get(`http://pokemap.randomain.name/pokemons/${Date.now()}`)
+        .auth('poke', 'mongo')
+        .end(async (err, res) => {
+          if (err) {
+            console.log(err)
+            return
+          }
+
+          let pokemons = res.body
+          let newPokemons = 0
+
+          for (let pokemon of pokemons) {
+            if (!pokemon.encounter_id) return
+            let found = await storage.findBy('pokemon_appears', 'encounter_id', pokemon.encounter_id)
+            if (found.length === 0) {
+              newPokemons++
+              await storage.insert('pokemon_appears', pokemon)
+            }
+          }
+
+          if (newPokemons > 0) {
+            console.log(`${Date()} Added ${newPokemons} new pokemon(s) from POKEMAP.RANDOMAIN.NAME`)
           }
 
           storage.close()
